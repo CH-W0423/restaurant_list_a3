1f972415d6e6902cbc531d50da2a2ce61636ca28
Liam Wang <pcnubhee@gmail.com>
2020-09-26 21:17:29 +0800

    project in

---
+++ restaurant_list_a3/README.md
@@ -0,0 +1,37 @@
+## 環境建置與需求 (prerequisites)
+
+---
+
+- npm: 6.4.1
+- Nodemon: 2.0.4
+- express: 4.17.1
+- Node.js: v10.15.3
+- express-handlebars: 5.1.0
+
+## 安裝與執行步驟 (installation and execution)
+
+---
+
+- npm run dev
+- npm isntall nodemon
+- 在瀏覽器網址輸入 http://localhost:3000
+
+## 功能描述 (features)
+
+---
+
+- 使用者可以在首頁看到所有餐廳與它們的簡單資料：
+
+* 餐廳照片
+* 餐廳名稱
+* 餐廳分類
+* 餐廳評分
+
+- 使用者可以再點進去看餐廳的詳細資訊：
+
+* 類別
+* 地址
+* 電話
+* 描述
+* 圖片
+* 使用者可以透過搜尋餐廳名稱來找到特定的餐廳
---
+++ restaurant_list_a3/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+// routes setting
+app.get("/", (req, res) => {
+  // past the movie data into 'index' partial template
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return (
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    );
+  });
+  res.render("index", { restaurants: restaurants });
+});
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`);
+});
